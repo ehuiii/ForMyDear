af0e77f0bb9e3e73d6fff99510b6aeb0839fa755
Siwon <73284904+tldnjs324@users.noreply.github.com>
2022-06-12 03:10:17 +0900

    category Modify

--- app/src/main/java/com/cookandroid/formydear/CategoryActivity.kt
+++ app/src/main/java/com/cookandroid/formydear/CategoryActivity.kt
@@ -18,12 +18,27 @@ class CategoryActivity: AppCompatActivity() {
 
     private var categoryList = arrayListOf<CategoryData>(
         CategoryData("즐겨찾기", "imgstar"),
-        CategoryData("인사 예절", "imgmanner"),
+        CategoryData("인사•예절", "imgmanner"),
         CategoryData("감정", "imgemotion"),
         CategoryData("음식", "imgfood"),
+        CategoryData("식기", "imgcutlery"),
         CategoryData("놀이", "imgplay"),
-        CategoryData("전자기기", "imgtv"),
-        CategoryData("장소", "imgplace")
+        CategoryData("아플 때", "imghurt"),
+        CategoryData("신체", "imgleg"),
+        CategoryData("가족•사람", "imgfamily"),
+        CategoryData("장소", "imgplace"),
+        CategoryData("숫자", "imgnumber"),
+        CategoryData("돈", "imgmoney"),
+        CategoryData("색깔", "imgcolor"),
+        CategoryData("모양", "imgshape"),
+        CategoryData("날씨", "imgweather"),
+        CategoryData("계절", "imgseason"),
+        CategoryData("동물", "imganimal"),
+        CategoryData("위치", "imgposition"),
+        CategoryData("도구", "imgpencil"),
+        CategoryData("가구", "imgtv"),
+        CategoryData("기타", "imgetc")
+
 
 
     )
